5ebc51e2191b47e7b88f76ab8ca7a03e0b985367
Kmyrrpen <shanenescarimoro@gmail.com>
2022-11-30 20:33:30 +0800

    initial

---
+++ index.ts
@@ -0,0 +1,15 @@
+import createStore from "zustand/vanilla";
+import { persist } from "zustand/middleware";
+import { immer } from "zustand/middleware/immer";
+
+interface State {
+  foo: boolean
+}
+
+createStore<State>()(immer((set, get) => ({
+  foo: true
+})));
+
+createStore<State>()(persist((set) => ({
+  foo: true
+}), { name: 'foo' }))
