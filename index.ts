b34b5059ee28f8a5cc4e238ec31e7772ca7929df
Kmyrrpen <shanenescarimoro@gmail.com>
2022-11-30 21:09:47 +0800

    edit

--- index.ts
+++ index.ts
@@ -3,13 +3,20 @@ import { persist } from "zustand/middleware";
 import { immer } from "zustand/middleware/immer";
 
 interface State {
-  foo: boolean
+  foo: boolean;
 }
 
-createStore<State>()(immer((set, get) => ({
-  foo: true
-})));
+createStore<State>()(
+  immer((set, get) => ({
+    foo: true,
+  }))
+);
 
-createStore<State>()(persist((set) => ({
-  foo: true
-}), { name: 'foo' }))
+createStore<State>()(
+  persist(
+    (set) => ({
+      foo: true,
+    }),
+    { name: "foo" }
+  )
+);
